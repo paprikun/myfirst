de60f27bd57628ccb4b20fe78a74e1f5077321ba
paprikun <32676078+paprikun@users.noreply.github.com>
2017-10-19 16:37:30 +0300

    new selector

--- step_definitions/steps.js
+++ step_definitions/steps.js
@@ -18,16 +18,19 @@ defineSupportCode(({ Given, Then, When }) => {
   });
   
   When(/^I find film "(.*?)"$/, (name) => {
-    return client.assert.visible('input[name=kp_query]')
-	  .setValue('input[name=kp_query]', [name, client.Keys.ENTER])
+		client.waitForElementVisible('input[name=kp_query]',1000);
+	  return client.setValue('input[name=kp_query]', name);
   });
  
    When(/^Check that rating is visible$/, () => {
-	return client.assert.visible('div[class=block_left_pad] :nth-child(4) div[class=right]');
+	client.pause(1000);
+	return client.waitForElementVisible("div[class*='first'] > a > div[class*='positive']",1000);
   });
   
   Then(/^Film rating is higher than (.[0-9]?)$/, (data) => {	
-	return client.getText("div[class=block_left_pad] :nth-child(4) div[class=right] :first-child", function(result) {
+  //div[class*='first'] > a > div[class*='positive']
+  //a[href='/film/453397/sr/2/'] > div:last-child
+	return client.getText("div[class*='first'] > a > div[class*='positive']", function(result) {
 		var rate = parseFloat(result.value);
 		console.log(rate);
 		console.log(data);
