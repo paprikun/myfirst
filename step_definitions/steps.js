554db86111922930381cff01b2a14ba0617f5d3b
paprikun <32676078+paprikun@users.noreply.github.com>
2017-10-19 13:41:31 +0300

    update step.js
    Разбил по шагам

--- step_definitions/steps.js
+++ step_definitions/steps.js
@@ -17,16 +17,21 @@ defineSupportCode(({ Given, Then, When }) => {
       .url('https://www.kinopoisk.ru');
   });
   
-   Then(/^Рейтинг фильма больше (.[0-9]?)$/, (data) => {
-       client.assert.visible('input[name=kp_query]')
-	   .setValue('input[name=kp_query]', ['Оно', client.Keys.ENTER])
-       .assert.visible('div[class=block_left_pad] :nth-child(4) div[class=right]');
-
+  When(/^I find film "(.*?)"$/, (name) => {
+    return client.assert.visible('input[name=kp_query]')
+				 .setValue('input[name=kp_query]', [name, client.Keys.ENTER])
+  });
+ 
+  When(/^Check that rating is visible$/, () => {
+		return client.assert.visible('div[class=block_left_pad] :nth-child(4) div[class=right]');
+  });
+  
+  Then(/^Film rating is higher than (.[0-9]?)$/, (data) => {	
 		return client.getText("div[class=block_left_pad] :nth-child(4) div[class=right] :first-child", function(result) {
-		 var rate = parseFloat(result.value);
-		 console.log(rate);
-		 console.log(data);
-		  client.assert.ok(rate > data )
+			var rate = parseFloat(result.value);
+			console.log(rate);
+			console.log(data);
+			client.assert.ok(rate > data )
 		});
   });
   
