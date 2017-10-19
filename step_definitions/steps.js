d9ceb01e220bdbbbfbef0b437253226bedb5bf7e
paprikun <32676078+paprikun@users.noreply.github.com>
2017-10-19 13:44:30 +0300

    Add files via upload

--- step_definitions/steps.js
+++ step_definitions/steps.js
@@ -19,20 +19,20 @@ defineSupportCode(({ Given, Then, When }) => {
   
   When(/^I find film "(.*?)"$/, (name) => {
     return client.assert.visible('input[name=kp_query]')
-				 .setValue('input[name=kp_query]', [name, client.Keys.ENTER])
+	  .setValue('input[name=kp_query]', [name, client.Keys.ENTER])
   });
  
    When(/^Check that rating is visible$/, () => {
-		return client.assert.visible('div[class=block_left_pad] :nth-child(4) div[class=right]');
+	return client.assert.visible('div[class=block_left_pad] :nth-child(4) div[class=right]');
   });
   
   Then(/^Film rating is higher than (.[0-9]?)$/, (data) => {	
-		return client.getText("div[class=block_left_pad] :nth-child(4) div[class=right] :first-child", function(result) {
-			var rate = parseFloat(result.value);
-			console.log(rate);
-			console.log(data);
-			client.assert.ok(rate > data )
-		});
+	return client.getText("div[class=block_left_pad] :nth-child(4) div[class=right] :first-child", function(result) {
+		var rate = parseFloat(result.value);
+		console.log(rate);
+		console.log(data);
+		client.assert.ok(rate > data )
+	});
   });
   
   Then(/^the title is "(.*?)"$/, (text) => {
