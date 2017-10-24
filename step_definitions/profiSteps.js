eea52dde960a7646b23408e5f9334598649f2da0
paprikun <paprikun@gmail.com>
2017-10-24 20:48:31 +0400

    задание по проверки цен (webdriver api, page object)

--- step_definitions/profiSteps.js
+++ step_definitions/profiSteps.js
@@ -41,14 +41,13 @@ defineSupportCode(({ Given, Then, When }) => {
   }); 
 
   When (/^собрать цены в массив$/, () => {
+  	 let sel = client.page.profi().section.content.elements.price.selector;
   	return client
-  	.elements('css selector', 'b.completed_orders__order__details__price', function(result) {
-
+  	.elements('css selector', sel, function(result) {
 		var els = result.value;
-        els.forEach(function(el, j){
-            client.elementIdText(el.ELEMENT, function(text) {
-                prices[j] = parseInt(text.value);
-                //console.log(prices[j]);
+        els.forEach(function(el){
+            client.elementIdText(el.ELEMENT, (text) => {
+                prices.push(parseInt(text.value));
             });
         });
 	
@@ -56,6 +55,9 @@ defineSupportCode(({ Given, Then, When }) => {
   });
   Then (/^проверить что все цены больше (.\d+?)$/, (value) => {
   	var check = true;
+  	if (prices.length == 0){
+  		check = false;
+  	}
   	prices.forEach(function(el, i){
   	  if (el < value){
   	  	check = false;
